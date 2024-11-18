c3d5e4217b6f2362e77c404a02f58bb6fd4566b4
Dominyka <dominyka.dobuzinskaite@gmail.com>
2024-11-18 12:46:03 +0200

    10reactuzduotis/

---
+++ timklapiai-kurimas/10reactuzduotis/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "./Header.css";
+export default function Header() {
+  return (
+    <header>
+      <h1 className="header-title">Page title</h1>
+      <img
+        src="\src\components\joe-caione-qO-PIF84Vxg-unsplash.jpg"
+        alt="Happy dog running on grass"
+        className="first"
+      />
+    </header>
+  );
+}
---
+++ timklapiai-kurimas/10reactuzduotis/src/components/PostContent.jsx
@@ -0,0 +1,14 @@
+import "./PostContent.css";
+export default function Main() {
+  return (
+    <main>
+      <h2>Post title</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, quisquam
+        et deserunt accusantium quas voluptas obcaecati possimus praesentium
+        laboriosam facilis, modi labore, veritatis aperiam. Assumenda magni
+        repellat sequi alias eos.
+      </p>
+    </main>
+  );
+}
