660fdf1fd7f9f915958a2ab95d690b6c344e9cfc
Dominyka <dominyka.dobuzinskaite@gmail.com>
2024-11-20 12:46:16 +0200

    16reactuzduotis/

---
+++ timklapiai-kurimas/16reactuzduotis/src/App.jsx
@@ -0,0 +1,10 @@
+import PostContent from "./components/PostContent";
+import PostsList from "./components/PostList";
+function App() {
+  return (
+    <div>
+      <PostsList />
+    </div>
+  );
+}
+export default App;
