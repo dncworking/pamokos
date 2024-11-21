5784ac6c9fe2fcd5cfea99a33a0d27877cc319e9
Dominyka <dominyka.dobuzinskaite@gmail.com>
2024-11-21 10:41:53 +0200

    18reactuzduotis/

---
+++ timklapiai-kurimas/18reactuzduotis/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Parent from "./component/Parent";
+
+function App() {
+  return (
+    <div>
+      <Parent />
+    </div>
+  );
+}
+export default App;
