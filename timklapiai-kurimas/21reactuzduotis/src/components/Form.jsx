835f460396183f4a9702b39267501a39a9ab709a
Dominyka <dominyka.dobuzinskaite@gmail.com>
2024-11-25 11:32:56 +0200

    21reactuzduotis/

---
+++ timklapiai-kurimas/21reactuzduotis/src/components/Form.jsx
@@ -0,0 +1,207 @@
+import { useForm } from "react-hook-form";
+export default function Form() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = async (data) => {
+    console.log(JSON.stringify(data));
+
+    try {
+      const response = await fetch("http://localhost:8080/animals", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <form className="col-4 mx-auto" onSubmit={handleSubmit(onSubmit)}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="form-control"
+          {...register("name")}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="type" className="form-label">
+          Type
+        </label>
+        <input
+          type="text"
+          id="type"
+          className="form-control"
+          {...register("type")}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="breed" className="form-label">
+          Breed
+        </label>
+        <input
+          type="text"
+          id="breed"
+          className="form-control"
+          {...register("breed")}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input
+          type="number"
+          id="age"
+          className="form-control"
+          {...register("age")}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="weight" className="form-label">
+          Weight
+        </label>
+        <input
+          type="number"
+          id="weight"
+          step="0.01"
+          className="form-control"
+          {...register("weight")}
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="gender" className="form-label">
+          Gender
+        </label>
+        <input
+          type="text"
+          id="gender"
+          className="form-control"
+          {...register("gender")}
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+}
+
+// return (
+//   <form className="col-4 mx-auto">
+//     <div className="mb-3">
+//       <label htmlFor="name" className="form-label">
+//         Name
+//       </label>
+//       <input type="text" id="name" className="form-control" />
+//     </div>
+
+//     <div className="mb-3">
+//       <label htmlFor="type" className="form-label">
+//         Type
+//       </label>
+//       <input type="text" id="type" className="form-control" />
+//     </div>
+
+//     <div className="mb-3">
+//       <label htmlFor="breed" className="form-label">
+//         Breed
+//       </label>
+//       <input type="text" id="breed" className="form-control" />
+//     </div>
+
+//     <div className="mb-3">
+//       <label htmlFor="age" className="form-label">
+//         Age
+//       </label>
+//       <input type="number" id="age" className="form-control" />
+//     </div>
+
+//     <div className="mb-3">
+//       <label htmlFor="weight" className="form-label">
+//         Weight
+//       </label>
+//       <input type="number" id="weight" step="0.01" className="form-control" />
+//     </div>
+
+//     <div className="mb-3">
+//       <label htmlFor="gender" className="form-label">
+//         Gender
+//       </label>
+//       <input type="text" id="gender" className="form-control" />
+//     </div>
+
+//     <button type="submit" className="btn btn-primary">
+//       Submit
+//     </button>
+//   </form>
+// );
+
+// import { useForm } from "react-hook-form";
+
+// export default function ReactHookFormExample() {
+//   const { register, handleSubmit } = useForm();
+
+//   const onSubmit = async (data) => {
+//     console.log(JSON.stringify(data));
+
+//     try {
+//       const response = await fetch("http://localhost:8080/register", {
+//         method: "POST",
+//         body: JSON.stringify(data),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       if (!response.ok) {
+//         throw new Error(`Response status: ${response.status}`);
+//       }
+//     } catch (error) {
+//       console.error(error);
+//     }
+//   };
+
+//   return (
+//     <form onSubmit={handleSubmit(onSubmit)}>
+//       <div>
+//         <label htmlFor="name">Name</label>
+//         <input
+//           type="text"
+//           placeholder="Name"
+//           id="name"
+//           {...register("name")}
+//         />
+//       </div>
+
+// //       <div>
+// //         <label htmlFor="email">Email</label>
+// //         <input
+//           type="email"
+//           placeholder="Email"
+//           id="email"
+//           {...register("email")}
+//         />
+//       </div>
+
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
